db98eb6be63c1e53b188dc9f70e968df3cb81ddd
nam1711-hub <hungnam171106@gmail.com>
2025-03-29 11:24:21 +0700

    Add project files.

---
+++ Project1/FileName.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int i = 0;
+    while (i <= n) {
+        n += i; 
+        i++;   
+    }
+    cout << "Giá trị cuối cùng của n: " << n << endl;
+    return 0;
+}
